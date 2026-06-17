aa8154926ba707acefa5065e0313b3875e672e8a
kevin <alexpenafiel27@gmail.com>
2026-06-16 19:38:32 -0500

    feat: add visitor counter to public site footer with session tracking

--- frontend/src/app/shared/footer/footer.ts
+++ frontend/src/app/shared/footer/footer.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { InfoGrupoService } from '../../core/services/info-grupo.service';
+import { VisitaService } from '../../core/services/visita.service';
 import { InfoGrupo } from '../../core/models/info-grupo.model';
 
 @Component({
@@ -14,10 +15,17 @@ import { InfoGrupo } from '../../core/models/info-grupo.model';
 export class FooterComponent implements OnInit {
   currentYear = new Date().getFullYear();
   info: InfoGrupo | null = null;
+  visitas: number | null = null;
 
-  constructor(private infoSvc: InfoGrupoService) {}
+  constructor(private infoSvc: InfoGrupoService, private visitaSvc: VisitaService) {}
 
   ngOnInit() {
     this.infoSvc.getInfoGrupo().subscribe({ next: (d) => { this.info = d; }, error: () => {} });
+    const sid = localStorage.getItem('reasons_sid') || crypto.randomUUID();
+    localStorage.setItem('reasons_sid', sid);
+    this.visitaSvc.registrarVisita(sid, window.location.pathname).subscribe({
+      next: (c) => { this.visitas = c; },
+      error: () => { this.visitas = 0; }
+    });
   }
 }
